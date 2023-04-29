28626329787a69bc3b128024bb3fcfb378db4542
yasminlopes <yclopes27@gmail.com>
2023-04-29 11:26:12 -0300

    feat: adicionar footer

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { RouterModule } from '@angular/router';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
     NavbarComponent,
+    FooterComponent,
   ],
   imports: [
     CommonModule,
@@ -15,6 +17,7 @@ import { RouterModule } from '@angular/router';
   ],
   exports: [
     NavbarComponent,
+    FooterComponent
   ]
 })
 export class CoreModule { }
