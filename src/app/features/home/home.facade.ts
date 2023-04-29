ab720d66d7f29ed589b5905f466861b20ff246c0
yasminlopes <yclopes27@gmail.com>
2023-04-29 17:40:52 -0300

    feat: implementar listagem do cardapio com o pattern facade

--- src/app/features/home/home.facade.ts
+++ src/app/features/home/home.facade.ts
@@ -1,6 +1,30 @@
+import { Injectable } from "@angular/core";
+import { CardapioApiService } from "./services/cardapio-api.service";
+import { CardapioState } from "./state/cardapio-state";
+import { EMPTY, Observable, catchError, map, tap } from "rxjs";
+import { Cardapio } from "./models/cardapio";
 import { ToastrService } from "ngx-toastr";
 
+@Injectable({ providedIn: 'root' })
+
+export class HomeFacade {
+  constructor(
+    private cardapioApiService: CardapioApiService,
+    private state: CardapioState,
     private toastr: ToastrService,
+    ) {}
+
+  public cardapio$ = this.state.cardapio;
+
+  public listarCardapio(): void {
+    this.cardapioApiService.listarCardapio().pipe(
+      map((resposta) => resposta),
+      catchError((erro) => this.notificarErro(erro))
+      ).subscribe((cardapio: Cardapio[]) => {
+      console.log(cardapio)
+      this.notificarSucesso(cardapio)
+    });
+  }
 
   private notificarSucesso(cardapio: Cardapio[]): void {
     this.state.cardapio.next(cardapio);
@@ -11,3 +35,6 @@ import { ToastrService } from "ngx-toastr";
     this.toastr.error('Oops... Algo deu errado.');
     return EMPTY;
   }
+  
+  
+}
