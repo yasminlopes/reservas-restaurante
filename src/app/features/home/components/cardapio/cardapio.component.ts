2072be3218681d9c53938f82613498a7482ae70a
yasminlopes <yclopes27@gmail.com>
2023-04-29 15:59:36 -0300

    feat: configurar swiper

---
+++ src/app/features/home/components/cardapio/cardapio.component.ts
@@ -0,0 +1,12 @@
+import Swiper from 'swiper';
+
+  ngAfterViewInit() {
+    new Swiper('.swiper-container', {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    });
+  }
