f8cb041922fd06385068d0132662b0d8ef184ee4
yasminlopes <yclopes27@gmail.com>
2023-04-29 11:33:28 -0300

    refactor: corrigir rota principal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,19 +2,27 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './core/layouts/main/main.component';
 import { CoreModule } from './core/core.module';
+import { FeaturesModule } from './features/features.module';
+import { HomeComponent } from './features/home/home.component';
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'home',
-    pathMatch: 'full',
-  },
-  {
-    path: 'home',
     component: MainComponent,
-    loadChildren: () => import('./core/core.module').then((m) => CoreModule),
+    children: [
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full',
+      },
+      {
+        path: 'home',
+        component: HomeComponent,
+      },
+    ],
   },
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
