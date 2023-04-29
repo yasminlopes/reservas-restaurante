3826f3eb7bd02d0080675a3a08c73cfe3a31d209
yasminlopes <yclopes27@gmail.com>
2023-04-29 16:00:42 -0300

    feat: criar componente de cardapio

--- src/app/features/home/components/cardapio/cardapio.component.ts
+++ src/app/features/home/components/cardapio/cardapio.component.ts
@@ -1,4 +1,20 @@
+import { Component, OnInit, Input } from '@angular/core';
 import Swiper from 'swiper';
+import { Cardapio } from '../../models/cardapio';
+
+@Component({
+  selector: 'app-cardapio',
+  templateUrl: './cardapio.component.html',
+  styleUrls: ['./cardapio.component.scss']
+})
+export class CardapioComponent implements OnInit {
+
+  @Input() cardapio: Cardapio[] | null;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 
   ngAfterViewInit() {
     new Swiper('.swiper-container', {
@@ -10,3 +26,6 @@ import Swiper from 'swiper';
       },
     });
   }
+  
+
+}
