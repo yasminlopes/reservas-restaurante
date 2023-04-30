57be23d966efbb320ae8a75dad8b7d8f8d1aa2ca
yasminlopes <yclopes27@gmail.com>
2023-04-30 17:54:51 -0300

    feat: adicionar evento de clique para navegar a rota de criar reserva

--- src/app/features/home/containers/home/home.component.ts
+++ src/app/features/home/containers/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HomeFacade } from '../../home.facade';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -8,10 +9,14 @@ import { HomeFacade } from '../../home.facade';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(public facade: HomeFacade) { }
+  constructor(public facade: HomeFacade, private router: Router) { }
 
   ngOnInit(): void {
     this.facade.listarCardapio()
+  }
+
+  public navegarParaCriarReserva(){
+    this.router.navigateByUrl('/criar-reserva')
   }
 
 }
