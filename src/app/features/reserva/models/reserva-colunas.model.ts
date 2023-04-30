67296e55461c8f201eb1d5bd7c9d196923517482
yasminlopes <yclopes27@gmail.com>
2023-04-30 17:52:26 -0300

    feat: definir colunas da tabela de reservas

---
+++ src/app/features/reserva/models/reserva-colunas.model.ts
@@ -0,0 +1,11 @@
+import { IColunas } from "src/app/shared/interfaces/colunas.interfaces";
+
+export const COLUNAS_LISTAGEM_RESERVAS: IColunas[] = [
+  { nome: 'Número Reserva'},
+  { nome: 'Responsável Reserva'},
+  { nome: 'Data e Hora'},
+  { nome: 'Status'},
+  { nome: 'Mesa'},
+  { nome: 'Total Pessoas'},
+  { nome: 'Detalhes'},
+]
