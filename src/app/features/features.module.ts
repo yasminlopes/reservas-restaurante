9040277af06161220be31aabf25c4097e632964e
yasminlopes <yclopes27@gmail.com>
2023-04-30 17:57:17 -0300

    chore: adicionar importações necessárias

--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -3,20 +3,28 @@ import { CommonModule } from '@angular/common';
 import { CoreModule } from '../core/core.module';
 import { HomeComponent } from './home/containers/home/home.component';
 import { CardapioComponent } from './home/components/cardapio/cardapio.component';
+import { ReservaComponent } from './reserva/containers/reserva/reserva.component';
+import { ReservaChatComponent } from './reserva/containers/reserva-chat/reserva-chat.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { ReservaListagemComponent } from './reserva/components/reserva-listagem/reserva-listagem.component';
 import { LoginComponent } from './login/container/login/login.component';
+import { ReservaHistoricoComponent } from './reserva/components/reserva-historico/reserva-historico.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
+    CardapioComponent,
     ReservaComponent,
-    ReservaHistoricoComponent
     ReservaChatComponent,
     ReservaListagemComponent,
     LoginComponent,
+    ReservaHistoricoComponent
   ],
   imports: [
     CommonModule,
-    CoreModule
+    CoreModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   exports: [
     HomeComponent
