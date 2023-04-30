04160ddfddad237cb8c0e394f691f14f17325dac
yasminlopes <yclopes27@gmail.com>
2023-04-30 16:08:15 -0300

    feat: criar interface de Mensagem

--- src/app/features/reserva/containers/reserva-chat/reserva-chat.component.ts
+++ src/app/features/reserva/containers/reserva-chat/reserva-chat.component.ts
@@ -2,6 +2,10 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { OpenAiService } from 'src/app/shared/services/open-ai.service';
 
+interface Mensagem {
+  conteudoMensagem: string;
+  chatBot: boolean;
+}
 
 @Component({
   selector: 'app-reserva-chat',
